c2f689e26538f94a8305e7bf9479cd2fdf2f539c
dennoknight <denniscoman108@gmail.com>
2024-10-04 16:41:18 +0300

    Add files via upload

---
+++ myassignment.java
@@ -0,0 +1,26 @@
+package javaassignment;
+import java.util.Scanner;
+
+public class Javaassignment {
+
+	public static void main (String[] args) {
+		int z=0;
+		while (z<=5)
+		{
+			System.out.println(z);
+			z++;
+		}
+		int r = 0;
+		do
+		{
+			System.out.println(r);
+			r++;
+		}while(r<=7);
+		
+		
+				 
+			
+
+	}
+
+}
